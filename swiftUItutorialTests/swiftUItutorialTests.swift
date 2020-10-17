40bf671e88e20570a491cad55e1c2cbccc0bf9ef
Pablo Trabajos <hz302783@hertz.net>
2020-10-17 17:09:38 +0100

    Initial Commit

---
+++ swiftUItutorialTests/swiftUItutorialTests.swift
@@ -0,0 +1,33 @@
+//
+//  swiftUItutorialTests.swift
+//  swiftUItutorialTests
+//
+//  Created by Pablo Tamayo on 17/10/2020.
+//
+
+import XCTest
+@testable import swiftUItutorial
+
+class swiftUItutorialTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
